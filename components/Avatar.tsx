d8b1a14cdfe358fa9942f9b15b198519a3d51427
king3 <w2196592083@gmail.com>
2025-12-17 09:38:31 +0000

    feat: add local and remote avatar URLs

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -7,7 +7,11 @@ interface AvatarProps {
   style?: React.CSSProperties
 }
 
-const AVATAR_SRC = '/images/king3.me.jpg'
+const LOCAL_AVATAR_LINK = '/images/king3.me.jpg'
+const REMOTE_AVATAR_LINK =
+  'https://i0.hdslb.com/bfs/openplatform/5b3f3a880d7b9265c703e96e4c6dad28b22b91fc.jpg'
+
+const AVATAR_Sx = REMOTE_AVATAR_LINK
 const AVATAR_ALT = 'King3'
 
 function Avatar({ size = 25, rounded = 99, style }: AvatarProps) {
