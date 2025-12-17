018dd08d8b387400748291c02a53c933b8fb6fe8
king3 <w2196592083@gmail.com>
2025-12-17 09:39:03 +0000

    fix

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -11,7 +11,7 @@ const LOCAL_AVATAR_LINK = '/images/king3.me.jpg'
 const REMOTE_AVATAR_LINK =
   'https://i0.hdslb.com/bfs/openplatform/5b3f3a880d7b9265c703e96e4c6dad28b22b91fc.jpg'
 
-const AVATAR_Sx = REMOTE_AVATAR_LINK
+const AVATAR_SRC = REMOTE_AVATAR_LINK
 const AVATAR_ALT = 'King3'
 
 function Avatar({ size = 25, rounded = 99, style }: AvatarProps) {
