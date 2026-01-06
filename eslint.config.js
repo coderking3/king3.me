97e9af33343fab455c97a510410e78231aedd177
coderking3 <2196592083@qq.com>
2026-01-06 21:37:42 +0800

    fix git err

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from '@king-3/eslint-config'
+
+export default defineConfig(
+  {
+    typescript: true,
+    nextjs: true,
+    ignores: ['**/(payload)/**/importMap.js']
+  },
+  {
+    name: 'custom-rules',
+    rules: {
+      'react-refresh/only-export-components': 'off',
+      'typescript/no-use-before-define': [
+        'error',
+        { classes: false, functions: false, variables: false }
+      ]
+    }
+  },
+
+  // 忽略对payload自动生成类型文件的进行校验
+  {
+    files: ['./payload-types.ts'],
+    name: 'payload-type-rule',
+    rules: {
+      'eslint-comments/no-unlimited-disable': 'off'
+    }
+  }
+)
