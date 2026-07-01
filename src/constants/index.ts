0b9c0d04ae445f6b351903e2a943a4231aee8516
coderking3 <2196592083@qq.com>
2026-07-02 01:23:12 +0800

    feat: update gallery images for enhanced visual content

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -46,12 +46,12 @@ export const AUTHOR: AuthorInfo = {
 /* Gallery */
 
 export const GALLERYS: string[] = [
-  'https://i0.hdslb.com/bfs/openplatform/4ab5f8290c9281aea91d6385b62f97eb1c0a03c2.png',
-  'https://i0.hdslb.com/bfs/openplatform/47696dfbc15966645d7d3374306e1e0976cb48b3.jpg',
-  'https://i0.hdslb.com/bfs/openplatform/110e6144aef41a2b01a49e2f040ff979c0e125eb.jpg',
-  'https://i0.hdslb.com/bfs/openplatform/8c8b12e1097d36605790a7a91e8e98968f7589f4.jpg',
-  'https://i0.hdslb.com/bfs/openplatform/36198b605b39580545fda23c8eac41be811332a1.jpg',
-  'https://i0.hdslb.com/bfs/openplatform/a7bbb2fdcb3a038410a53d123cdd54dbf700090a.png'
+  'http://i0.hdslb.com/bfs/openplatform/d1e5599055b967325d51d45745e57d77e8af0eb8.jpg',
+  'http://i0.hdslb.com/bfs/openplatform/521430cee97678bac9fcbb8e6b5477fe2c645012.jpg',
+  'http://i0.hdslb.com/bfs/openplatform/9d18919881eccefd9789ad4ee1f48c1ae1aa7114.jpg',
+  'https://i0.hdslb.com/bfs/openplatform/769f79ee86aed7b14eca863700a6eb9ca19caaa3.jpg',
+  'http://i0.hdslb.com/bfs/openplatform/659662ae007ee9a54c5e95ee6c442e5c34cf5d32.jpg',
+  'http://i0.hdslb.com/bfs/openplatform/e1e6aaf10448b1136da1354e3b7e023d4344e963.jpg'
 ]
 
 export * from './animation'
